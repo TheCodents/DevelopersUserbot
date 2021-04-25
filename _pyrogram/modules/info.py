65c9351ab1dc89a844099bf5a256addf29afa8b0
sᴡᴏɴɪᴛ <swatv3nub@gmail.com>
2021-04-25 16:37:54 +0530

    Much Upgraded the ID module

--- _pyrogram/modules/info.py
+++ _pyrogram/modules/info.py
@@ -70,29 +70,42 @@ async def whois(client, message):
 
 @app.on_message(filters.command("id", PREFIX) & filters.me)
 async def id(client, message):
-    cmd = message.command
-    chat_id = message.chat.id
-    if not message.reply_to_message and len(cmd) == 1:
-        get_user = message.from_user.id
-    elif len(cmd) == 1:
-        get_user = message.reply_to_message.from_user.id
-    elif len(cmd) > 1:
-        get_user = cmd[1]
-        msg_id = message.message_id
-        try:
-            get_user = int(cmd[1])
-        except ValueError:
-            pass
-    try:
-        user = await client.get_users(get_user)
-    except Exception as e:
-        await message.edit(f"{e}")
-        return
+    text_unping = '<b>Chat ID:</b>'
     if message.chat.username:
-        chat_78 = f"**[Chat ID](https://t.me/{message.chat.username})**: `{message.chat.id}`"
-    else:
-        chat_78 = f"**Chat ID**: `{message.chat.id}`"
-    text = f"""{chat_78} \n**[Message ID]({message.link})**: `{message.message_id}`\n**[User ID](tg://user?id={user.id})**: `{user.id}`\n"""
-    if len(cmd) == 1:
-        text += f"\n**[Replied Message ID]({message.reply_to_message.link})**: `{message.reply_to_message.message_id}` \n**[Replied User ID](tg://user?id={message.reply_to_message.from_user.id})**: `{message.reply_to_message.from_user.id}`"
-    await message.edit(text, disable_web_page_preview=True)
+        text_unping = f'<a href="https://t.me/{message.chat.username}">{text_unping}</a>'
+    text_unping += f' <code>{message.chat.id}</code>\n'
+    text = '<b>Message ID:</b>'
+    if message.link:
+        text = f'<a href="{message.link}">{text}</a>'
+    text += f' <code>{message.message_id}</code>\n'
+    text_unping += text
+    if message.from_user:
+        text_unping += f'<b><a href="tg://user?id={message.from_user.id}">User ID:</a></b> <code>{message.from_user.id}</code>\n'
+    text_ping = text_unping
+    reply = message.reply_to_message
+    if not getattr(reply, 'empty', True):
+        text_unping += '\n'
+        text = '<b>Replied Message ID:</b>'
+        if reply.link:
+            text = f'<a href="{reply.link}">{text}</a>'
+        text += f' <code>{reply.message_id}</code>\n'
+        text_unping += text
+        text_ping = text_unping
+        if reply.from_user:
+            text = '<b>Replied User ID:</b>'
+            if reply.from_user.username:
+                text = f'<a href="https://t.me/{reply.from_user.username}">{text}</a>'
+            text += f' <code>{reply.from_user.id}</code>\n'
+            text_unping += text
+            text_ping += f'<b><a href="tg://user?id={reply.from_user.id}">Replied User ID:</a></b> <code>{reply.from_user.id}</code>\n'
+        if reply.forward_from:
+            text_unping += '\n'
+            text = '<b>Forwarded User ID:</b>'
+            if reply.forward_from.username:
+                text = f'<a href="https://t.me/{reply.forward_from.username}">{text}</a>'
+            text += f' <code>{reply.forward_from.id}</code>\n'
+            text_unping += text
+            text_ping += f'\n<b><a href="tg://user?id={reply.forward_from.id}">Forwarded User ID:</a></b> <code>{reply.forward_from.id}</code>\n'
+    reply = await message.reply_text(text_unping, disable_web_page_preview=True)
+    if text_unping != text_ping:
+        await reply.edit_text(text_ping, disable_web_page_preview=True)
