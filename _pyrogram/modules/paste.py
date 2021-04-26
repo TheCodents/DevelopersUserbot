e5a11067fea3bb42e88ccc5b136f5c2fccd998f1
ᴊᴀʏᴀиᴛ ʜᴇɢᴅᴇ ᴋᴀɢᴇяɪ <69081616+jayantkageri@users.noreply.github.com>
2021-04-26 09:18:24 +0530

    Fix Paste

--- _pyrogram/modules/paste.py
+++ _pyrogram/modules/paste.py
@@ -3,11 +3,12 @@
 # (C) 2021 Jayant Kageri
 
 import asyncio
-
 import aiohttp
-from pyrogram import filters
-from pyrogram.types import Message
-from _pyrogram import app, CMD_HELP
+import json
+import os
+from urllib.parse import urlparse
+from pyrogram import Client, filters
+from _pyrogrm import app, CMD_HELP
 from config import PREFIX
 
 CMD_HELP.update(
@@ -24,27 +25,90 @@ CMD_HELP.update(
     }
 )
 
+TMP_DOWNLOAD_DIRECTORY = "./_pyrogram/"
 
-SCHEMA = "https"
-BASE = "nekobin.com"
-ENDPOINT = f"{SCHEMA}://{BASE}/api/documents"
-ANSWER = "Pasted to Nekobin \n{}"
-TIMEOUT = 3
-MESSAGE_ID_DIFF = 100
+@apl.on_message(filters.command("paste", PREFIX)
+async def paste_bin(_, message):
+    status_message = await message.reply_text("...")
+    downloaded_file_name = None
 
-
-@app.on_message(
-    filters.command(["neko", "nekobin", "bin", "paste"], PREFIX) & filters.me
-)
-async def neko(_, message: Message):
-    reply = message.reply_to_message
-    if not reply:
+    if message.reply_to_message and message.reply_to_message.media:
+        downloaded_file_name_res = await message.reply_to_message.download(
+            file_name=TMP_DOWNLOAD_DIRECTORY
+        )
+        m_list = None
+        with open(downloaded_file_name_res, "rb") as fd:
+            m_list = fd.readlines()
+        downloaded_file_name = ""
+        for m in m_list:
+            downloaded_file_name += m.decode("UTF-8")
+        os.remove(downloaded_file_name_res)
+    elif message.reply_to_message:
+        downloaded_file_name = message.reply_to_message.text.html
+    # elif len(message.command) > 1:
+    #     downloaded_file_name = " ".join(message.command[1:])
+    else:
+        await status_message.edit("Not said What to do")
         return
+
+    if downloaded_file_name is None:
+        await status_message.edit("Didn't say what to do")
+        return
+
+    json_paste_data = {
+        "content": downloaded_file_name
+    }
+
+    # a dictionary to store different pastebin URIs
+    paste_bin_store_s = {
+        "deldog": "https://del.dog/documents",
+        "nekobin": "https://nekobin.com/api/documents"
+    }
+
+    chosen_store = "nekobin"
+    if len(message.command) == 2:
+        chosen_store = message.command[1]
+
+    # get the required pastebin URI
+    paste_store_url = paste_bin_store_s.get(
+        chosen_store,
+        paste_bin_store_s["nekobin"]
+    )
+    paste_store_base_url_rp = urlparse(paste_store_url)
+
+    # the pastebin sites, respond with only the "key"
+    # we need to prepend the BASE_URL of the appropriate site
+    paste_store_base_url = paste_store_base_url_rp.scheme + "://" + \
+        paste_store_base_url_rp.netloc
+
     async with aiohttp.ClientSession() as session:
-        async with session.post(
-            ENDPOINT,
-            json={"content": reply.text},
-            timeout=TIMEOUT
-        ) as response:
-            key = (await response.json())["result"]["key"]
-    await message.edit(ANSWER.format(f"{BASE}/{key}.py"), disable_web_page_preview=True)
+        response_d = await session.post(paste_store_url, json=json_paste_data)
+        response_jn = await response_d.json()
+
+    # we got the response from a specific site,
+    # this dictionary needs to be scrapped
+    # using bleck megick to find the "key"
+    t_w_attempt = bleck_megick(response_jn)
+    required_url = json.dumps(
+        t_w_attempt, sort_keys=True, indent=4
+    ) + "\n\n #ERROR"
+    if t_w_attempt is not None:
+        required_url = paste_store_base_url + "/" + "raw" + "/" + t_w_attempt
+
+    await status_message.edit(required_url)
+
+
+def bleck_megick(dict_rspns):
+    # first, try getting "key", dirctly
+    first_key_r = dict_rspns.get("key")
+    # this is for the "del.dog" site
+    if first_key_r is not None:
+        return first_key_r
+    check_if_result_ests = dict_rspns.get("result")
+    if check_if_result_ests is not None:
+        # this is for the "nekobin.com" site
+        second_key_a = check_if_result_ests.get("key")
+        if second_key_a is not None:
+            return second_key_a
+    # TODO: is there a better way?
+    return None
