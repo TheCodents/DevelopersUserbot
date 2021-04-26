4ce5f76c7351027713c60a3eae3d200bda6c7805
ᴊᴀʏᴀиᴛ ʜᴇɢᴅᴇ ᴋᴀɢᴇяɪ <69081616+jayantkageri@users.noreply.github.com>
2021-04-26 09:04:20 +0530

    Update paste.py

--- _pyrogram/modules/paste.py
+++ _pyrogram/modules/paste.py
@@ -1,4 +1,4 @@
-# This file is Originally Written By @okay-retard on GitHub
+# This file is taken from @pyrogram on GitHub
 # The Author (Jayant Kageri) just Ported this for Devloper Userbot
 # (C) 2021 Jayant Kageri
 
@@ -25,42 +25,26 @@ CMD_HELP.update(
 )
 
 
+SCHEMA = "https"
+BASE = "nekobin.com"
+ENDPOINT = f"{SCHEMA}://{BASE}/api/documents"
+ANSWER = "Pasted to Nekobin \n{}"
+TIMEOUT = 3
+MESSAGE_ID_DIFF = 100
+
+
 @app.on_message(
     filters.command(["neko", "nekobin", "bin", "paste"], PREFIX) & filters.me
 )
-async def paste(_, message: Message):
-    text = message.reply_to_message.text
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.post(
-                "https://nekobin.com/api/documents", json={"content": text}, timeout=3
-            ) as response:
-                key = (await response.json())["result"]["key"]
-    except Exception:
-        await message.edit_text("`Pasting failed`")
-        await asyncio.sleep(2)
-        await message.delete()
+async def neko(_, message: Message):
+    reply = message.reply_to_message
+    if not reply:
         return
-    else:
-        url = f"https://nekobin.com/{key}"
-        reply_text = f"Pasted to Nekobin : {url}"
-        delete = (
-            True
-            if len(message.command) > 1
-            and message.command[1] in ["d", "del"]
-            and message.reply_to_message.from_user.is_self
-            else False
-        )
-        if delete:
-            await asyncio.gather(
-                app.send_message(
-                    message.chat.id, reply_text, disable_web_page_preview=True
-                ),
-                message.reply_to_message.delete(),
-                message.delete(),
-            )
-        else:
-            await message.edit_text(
-                reply_text,
-                disable_web_page_preview=True,
-            )
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            ENDPOINT,
+            json={"content": reply.text},
+            timeout=TIMEOUT
+        ) as response:
+            key = (await response.json())["result"]["key"]
+    await message.edit(ANSWER.format(f"{BASE}/{key}.py"))
