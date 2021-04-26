4375d1f9fa72700b8c86844338581e5212054c73
ᴊᴀʏᴀиᴛ ʜᴇɢᴅᴇ ᴋᴀɢᴇяɪ <69081616+jayantkageri@users.noreply.github.com>
2021-04-26 11:26:17 +0530

    Update __main__.py

--- _pyrogram/__main__.py
+++ _pyrogram/__main__.py
@@ -3,6 +3,9 @@
 from pyrogram import idle, Client, filters
 from _pyrogram import app, LOGGER
 from _pyrogram.modules import *
+import os
+
+os.system("clear")
 
 print("Pyrogram User Client Started \nPowered By @TheCodents\n(C) 2021 Jayant Kageri")
 app.run()
