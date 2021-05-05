1301555864975ed30c61d90ac93f324b7218ab2e
ᴊᴀʏᴀиᴛ ʜᴇɢᴅᴇ ᴋᴀɢᴇяɪ <69081616+jayantkageri@users.noreply.github.com>
2021-05-05 21:13:35 +0530

    Fix

--- _pyrogram/modules/json.py
+++ _pyrogram/modules/json.py
@@ -2,10 +2,31 @@ from pyrogram.types import *
 from _pyrogram import *
 from pyrogram import *
 from config import *
+from os import *
 
 @app.on_message(filters.command("json", PREFIX) & filters.me)
 async def start(client, message):
-	if message.reply_to_message:
-		await message.reply(message.reply_to_message)
-	else:
-		await message.reply(message)
+    try:
+        if message.reply_to_message:
+            msg_info = message.reply_to_message
+
+        else:
+            msg_info = message
+
+        if len(msg_info) > 4096:
+            file = open("json.txt", "w+")
+            file.write(msg_info)
+            file.close()
+            await app.send_document(
+                message.chat.id,
+                "json.txt",
+                caption="Returned JSon",
+
+            )
+            remove("json.txt")
+
+        else:
+            await message.edit(msg_info)
+
+    except Exception as e:
+        await message.edit(f"{e}")
