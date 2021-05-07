9d8ba286c1f2a8077f9beeb9d4afbbc72facca65
Error <error@notavailable.live>
2021-05-07 09:14:41 +0530

    IDK if works

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import multiprocessing
 from _telethon import app as tapp
 from _pyrogram import app as papp
     
-def prun(pclient, **args):# kinda weird
+def prun(pclient, useless):# kinda weird
+    what = useless
     pclient.run()
     
 if __name__ == "__main__":
     multiprocessing.Process(target=prun, args=(papp, True)).start()
-    await tapp.connect()
+    # await tapp.connect()
     tapp.run_until_disconnected()
