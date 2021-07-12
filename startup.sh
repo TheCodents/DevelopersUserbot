5a24040125f9db99e1f43e41e901a908eb409508
Error <error@notavailable.live>
2021-07-12 08:01:28 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,13 +1,13 @@
 if [[ -z "$TELETHON_SESSION" && -z "$PYROGRAM_SESSION" ]]
 then
-	python -m _pyrogram && python -m _telethon
+	echo "Add PYROGRAM_SESSION or TELETHON_SESSION first!"
 elif [[ -z "$TELETHON_SESSION" ]]
 then
-	python -m _telethon
+	python -m _pyrogram
 elif [[ -z "$PYROGRAM_SESSION" ]]
 then
-	python -m _pyrogram
+	python -m _telethon
 else
-	echo "Add PYROGRAM_SESSION or TELETHON_SESSION first!"
+	python -m _pyrogram && python -m _telethon
 	exit 1
 fi
